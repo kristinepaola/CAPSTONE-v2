c04c4842d6ab5da88a86f9306de680ef5411bfc5
ofels <gabisayofeliajane@gmail.com>
2017-12-02 20:09:07 +0800

    ofels ni sis

--- admin/add_advocacy.php
+++
@@ -1,8 +0,0 @@
-<?php include("../sql_connect.php"); ?>
-<html>
-<form method="POST" action="insert_advocacy.php" enctype="multipart/form-data">
-  Advocacy Name: <input type="text" name="advocacy"><br>
-  Advocacy Icon: <input type="file" name="advocacyicon"><br>
-  <button type="submit" name="submit">Submit</button>
-</form>
-</html>
