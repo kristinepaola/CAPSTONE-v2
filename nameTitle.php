d03c52f3debfa0e7e757217430b70cb5fee3ff13
Eunice Valenzuela <euniceot12@gmail.com>
2017-12-02 15:22:50 +0800

    okok

---
+++ nameTitle.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+  <nav class="navbar navbar-default ">
+      <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse yamm" id="navigation">
+                <div class="button navbar-right">
+                  <div class="col-md-3 blg-thumb p0">
+                  <img src="1.2.jpg" height="5" width="130"></a>
+                  </div>
+                  <div class="col-md-8 blg-entry">
+                      <h6> <a href="single.html">Mary Eunice</a></h6>
+                      <img src="mail.png" height="5" width="30"></a>
+                  </div>
+                </div>
+                <ul class="main-nav nav navbar-nav navbar-right">
+                    <li class="wow fadeInDown" data-wow-delay="0.1s"><a class="" href="AboutUs.html">About Us</a></li>
+                    <li class="wow fadeInDown" data-wow-delay="0.1s"><a class="" href="EventPageHERE.html">Events</a></li>
+                    <li class="wow fadeInDown" data-wow-delay="0.4s"><a href="OrgPageHERE.html">Organization</a></li>
+                    <li class="dropdown yamm-fw" data-wow-delay="0.1s">
+                              <!-- /.yamm-content -->
+                          </li>
+                      </ul>
+                  </li>
+
+
+              </ul>
+          </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+  </nav>
+  <!-- End of nav bar -->
+</head>
+</html>
