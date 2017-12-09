7caa474cec79f32fe15eaf57f0ccde483352bb34
daphnemaine <33106234+daphnemaine@users.noreply.github.com>
2017-12-09 15:04:42 +0800

    insert occupation query

--- Organization/insert_occ_event.php
+++ Organization/insert_occ_event.php
@@ -4,5 +4,17 @@ require ("../sql_connect.php");
 $occupationName = $_POST["occupationName"];
 $noVolunteers = $_POST["noVolunteers"];
 
-foreach ()
+foreach ($occupationName as $key => $value){
+  $addocc_query = "INSERT INTO occupation_event VALUES ('',
+                                      '".sql->.real_escape_string($value)"',
+                                      ''".sql->.real_escape_string($noVolunteers[$key])"')";
+
+$addocc_data = mysqli_query($sql, $addocc_query);
+if ($insert){
+  echo "<p>Stored to database</p>";
+}
+else{
+  echo $mysqli->error;
+}
+}
 ?>
