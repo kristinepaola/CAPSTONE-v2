883cbfe388d3ad5b83f1e6aad8b9231f411f2306
ofels <gabisayofeliajane@gmail.com>
2017-12-09 14:38:44 +0800

    add-delete occupation event

--- Organization/occ_event.php
+++ Organization/occ_event.php
@@ -4,17 +4,30 @@
 <html>
 <body>
   <form method="POST">
+    <label>Event Title <small>(required)</small></label>
+    <input name="eventTitle" type="text"  >
     <div id="container">
-      <input name="occupationName" type="text" id="occupation">
-      <input name="noVolunteers" type="number" id="noVolunteers">
+      Occupation Name:<input name="occupationName" type="text" id="occupation">
+      No. of Volunteers:<input name="noVolunteers" type="number" id="noVolunteers">
       <a href="#" id="add">Add</a>
     </div>
     <input type="submit" name="submit"/>
 </body>
 </html>
-<script src=".../assets/js/jquery-1.10.2.min.js"></script>
+<script src="../assets/js/jquery-1.10.2.min.js"></script>
 <script>
-$(document).ready(function(e){
+$(document).ready(function(){
+  //Variables
+   var display = '<p/><div id="container">Occupation Name:<input name="occupationName" type="text" id="occupation">No. of Volunteers:<input name="noVolunteers" type="number" id="noVolunteers"><a href="#" id="delete">Delete</a></div>';
+  //Add rows to the form
+  $("#add").click(function(){
+    $("#container").append(display);
+  });
+  //Remove
+  $("#container").on("click", "#delete", function(){
+    $(this).parent("div").remove();
+  });
 
-}
+
+});
 </script>
