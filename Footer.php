9f98653603a823c234ac916dcb394c863859f7e0
daphnemaine <33106234+daphnemaine@users.noreply.github.com>
2017-12-10 14:19:42 +0800

    deleted unecessary shit sa atong life
    gipang delete nako ang mga way gamit ka wa na silai ayo.
    
    the ff. file/s & folders were deleted:
    - index.php
    - bootstrap (naa ra ni sa sulod sa "-TEMPLATE" nga folder)
    - assests (naa ra ni sa sulod sa "-TEMPLATE" nga folder)

--- Footer.php
+++ Footer.php
@@ -1,90 +1,90 @@
 <!DOCTYPE html>
-    <body>
+<body>
 <!-- Footer area-->
-        <div class="footer-area">
+<div class="footer-area">
 
-            <div class=" footer">
-                <div class="container">
-                    <div class="row">
+<div class=" footer">
+<div class="container">
+<div class="row">
 
-                        <div class="col-md-3 col-sm-6 wow fadeInRight animated">
-                            <div class="single-footer">
-                                <h4>About us </h4>
-                                <div class="footer-title-line"></div>
+<div class="col-md-3 col-sm-6 wow fadeInRight animated">
+	<div class="single-footer">
+		<h4>About us </h4>
+		<div class="footer-title-line"></div>
 
-                                <img src="" alt="" class="wow pulse" data-wow-delay="1s">
-                                <p>Lorem ipsum dolor cum necessitatibus su quisquam molestias. Vel unde, blanditiis.</p>
-                                <ul class="footer-adress">
-                                    <li><i class="pe-7s-map-marker strong"> </i> 9089 your adress her</li>
-                                    <li><i class="pe-7s-mail strong"> </i> email@yourcompany.com</li>
-                                    <li><i class="pe-7s-call strong"> </i> +1 908 967 5906</li>
-                                </ul>
-                            </div>
-                        </div>
+		<img src="" alt="" class="wow pulse" data-wow-delay="1s">
+		<p>Lorem ipsum dolor cum necessitatibus su quisquam molestias. Vel unde, blanditiis.</p>
+		<ul class="footer-adress">
+			<li><i class="pe-7s-map-marker strong"> </i> 9089 your adress her</li>
+			<li><i class="pe-7s-mail strong"> </i> email@yourcompany.com</li>
+			<li><i class="pe-7s-call strong"> </i> +1 908 967 5906</li>
+		</ul>
+	</div>
+</div>
 
-                        <div class="col-md-3 col-sm-6 wow fadeInRight animated">
-                            <div class="single-footer news-letter">
-                                <h4>Stay in touch</h4>
-                                <div class="footer-title-line"></div>
-                                <p>Lorem ipsum dolor sit amet, nulla  suscipit similique quisquam molestias. Vel unde, blanditiis.</p>
+<div class="col-md-3 col-sm-6 wow fadeInRight animated">
+	<div class="single-footer news-letter">
+		<h4>Stay in touch</h4>
+		<div class="footer-title-line"></div>
+		<p>Lorem ipsum dolor sit amet, nulla  suscipit similique quisquam molestias. Vel unde, blanditiis.</p>
 
-                                <form>
-                                    <div class="input-group">
-                                        <input class="form-control" type="text" placeholder="E-mail ... ">
-                                        <span class="input-group-btn">
-                                            <button class="btn btn-primary subscribe" type="button"><i class="pe-7s-paper-plane pe-2x"></i></button>
-                                        </span>
-                                    </div>
-                                    <!-- /input-group -->
-                                </form>
+		<form>
+			<div class="input-group">
+				<input class="form-control" type="text" placeholder="E-mail ... ">
+				<span class="input-group-btn">
+					<button class="btn btn-primary subscribe" type="button"><i class="pe-7s-paper-plane pe-2x"></i></button>
+				</span>
+			</div>
+			<!-- /input-group -->
+		</form>
 
-                                <div class="social pull-right">
-                                    <ul>
-                                        <li><a class="wow fadeInUp animated" href="https://www.facebook.com/kimarotec" data-wow-delay="0.2s"><i class="fa fa-facebook"></i></a></li>
-                                        <li><a class="wow fadeInUp animated" href="https://plus.google.com/kimarotec" data-wow-delay="0.3s"><i class="fa fa-google-plus"></i></a></li>
-                                        </ul>
-                                </div>
-                            </div>
-                        </div>
+		<div class="social pull-right">
+			<ul>
+				<li><a class="wow fadeInUp animated" href="https://www.facebook.com/kimarotec" data-wow-delay="0.2s"><i class="fa fa-facebook"></i></a></li>
+				<li><a class="wow fadeInUp animated" href="https://plus.google.com/kimarotec" data-wow-delay="0.3s"><i class="fa fa-google-plus"></i></a></li>
+				</ul>
+		</div>
+	</div>
+</div>
 
-                    </div>
-                </div>
-            </div>
+</div>
+</div>
+</div>
 
-            <div class="footer-copy text-center">
-                <div class="container">
-                    <div class="row">
-                        <div class="pull-left">
-                            <span> (C) <a href="http://www.KimaroTec.com"></a> , All rights reserved 2016  </span>
-                        </div>
-                        <div class="bottom-menu pull-right">
-                            <ul>
-                                <li><a class="wow fadeInUp animated" href="#" data-wow-delay="0.2s">Home</a></li>
-                                <li><a class="wow fadeInUp animated" href="#" data-wow-delay="0.6s">Contact</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="footer-copy text-center">
+<div class="container">
+<div class="row">
+<div class="pull-left">
+	<span> (C) <a href="http://www.KimaroTec.com"></a> , All rights reserved 2016  </span>
+</div>
+<div class="bottom-menu pull-right">
+	<ul>
+		<li><a class="wow fadeInUp animated" href="#" data-wow-delay="0.2s">Home</a></li>
+		<li><a class="wow fadeInUp animated" href="#" data-wow-delay="0.6s">Contact</a></li>
+	</ul>
+</div>
+</div>
+</div>
+</div>
 
-        </div>
+</div>
 
-        <script src="assets/js/modernizr-2.6.2.min.js"></script>
+<script src="assets/js/modernizr-2.6.2.min.js"></script>
 
-        <script src="assets/js/jquery-1.10.2.min.js"></script>
-        <script src="bootstrap/js/bootstrap.min.js"></script>
-        <script src="assets/js/bootstrap-select.min.js"></script>
-        <script src="assets/js/bootstrap-hover-dropdown.js"></script>
+<script src="assets/js/jquery-1.10.2.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/js/bootstrap-select.min.js"></script>
+<script src="assets/js/bootstrap-hover-dropdown.js"></script>
 
-        <script src="assets/js/easypiechart.min.js"></script>
-        <script src="assets/js/jquery.easypiechart.min.js"></script>
+<script src="assets/js/easypiechart.min.js"></script>
+<script src="assets/js/jquery.easypiechart.min.js"></script>
 
-        <script src="assets/js/owl.carousel.min.js"></script>
-        <script src="assets/js/wow.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/wow.js"></script>
 
-        <script src="assets/js/icheck.min.js"></script>
-        <script src="assets/js/price-range.js"></script>
+<script src="assets/js/icheck.min.js"></script>
+<script src="assets/js/price-range.js"></script>
 
-        <script src="assets/js/main.js"></script>
-    </body>
+<script src="assets/js/main.js"></script>
+</body>
 </html>
