6b490c9e64f8b663d8ffcab0ddba5f4fe29a367b
Kristine Paola P. Nacasabog <32124617+kristinepaola@users.noreply.github.com>
2017-12-11 16:24:32 +0800

    Revert "wala lng ambot"
    This reverts commit 026c28f1e6e79c66f8f50278d5d3123be76dd5bb.

--- sql_connect.php
+++ sql_connect.php
@@ -1,19 +1,9 @@
 <?php
-<<<<<<< HEAD
 
 $sql = mysqli_connect ("localhost", "root", "", "webportal");
 
 if (!$sql){
 	echo "Error connecting to database<br>";
-=======
-$mysqli = mysqli_connect("localhost", "root", "", "webportal");
-/* check connection */
-
-if (!$mysqli) {
-  printf("Connect failed: %s\n", mysqli_connect_error());
-  echo "ERROR! Connection Failed!";
-  exit();
->>>>>>> duff-
 }
 
 ?>
