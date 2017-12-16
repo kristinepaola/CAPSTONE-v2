ddf7b53ffeb0588f42d7ea9ce6abdc32f926ad6e
daphnemaine <33106234+daphnemaine@users.noreply.github.com>
2017-12-16 15:40:39 +0800

    database namo ni ofels

--- Organization/insertOrganization.php
+++ Organization/insertOrganization.php
@@ -1,6 +1,8 @@
 <?php
 require ("../sql_connect.php");
-
+$email = $_POST['emailAdd'];
+$msg = "Test. You are registered!";
+$header = "From: daphnecomendador@gmail.com";
 if (isset($_POST['advocacy'])){
   $checkedAdv = $_POST['advocacy'];
   $impAdv = array();
@@ -13,8 +15,6 @@ if (isset($_POST['advocacy'])){
 }else{
   echo "select at least 1 advocacy";
 }
-
-
 $query = "INSERT INTO organization VALUES ('',
                           '".$_POST['orgName']."',
                           '".$_POST['emailAdd']."',
@@ -27,6 +27,7 @@ $data = mysqli_query($sql, $query);
 if (!$data){
   echo "ERROR IN QUERY";
 }else{
+  mail ($email, "Test", $msg, $header);
   header("organization_dashboard.php");
 }
 
