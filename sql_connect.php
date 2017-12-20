fcacfd7513a4cd212110962fcb6b02d6e7f5537e
Kristine Paola P. Nacasabog <32124617+kristinepaola@users.noreply.github.com>
2017-12-21 01:22:51 +0800

    Merge branch 'admin' into dev

--- sql_connect.php
+++ sql_connect.php
@@ -1,12 +1,4 @@
 <?php
-$mysqli = mysqli_connect("localhost", "root", "", "webportal");
-/* check connection */
-
-if (!$mysqli) {
-  printf("Connect failed: %s\n", mysqli_connect_error());
-  echo "ERROR! Connection Failed!";
-  exit();
-=======
 $sql = mysqli_connect("localhost", "root", "", "webportal");
 /* check connection */
 
@@ -14,7 +6,5 @@ if (!$sql) {
   printf("Connect failed: %s\n", mysqli_connect_error());
   echo "ERROR! Connection Failed!";
   exit();
->>>>>>> admin
 }
-
 ?>
